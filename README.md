9dfa7b12f996708390d4a107a9b3b5293f2d2aaf
aaronksaunders <aaron@clearlyinnovative.com>
2015-02-02 20:59:50 -0500

    additional edits and updates to the source code and the readme.md file

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-# AppC-Alloy-Book-Misc-Chp2
---
 
-This chapter requires that you have created an account in appcelerator cloud services, please follow the instructions from the chapter to setup your account
+Building Cross-Platform Apps using Titanium, Alloy, and Appcelerator Cloud Services
+====
+![Book Cover Image](http://ecx.images-amazon.com/images/I/519RGyDDVIL.jpg)
+### Notes on this sample code from Chapter Two
+
+This chapter requires that you have created an account in appcelerator cloud services, please follow the instructions from the chapter to setup your account,
+
+Make sure you set up a test user in the account associated with the application
+
+User email `wileyonetest@clearlyinnovative.com` and password `wileyonetest`. You can change these, but you must then update the sample application below.
+
+ key you have included in the `tiapp.xml` file
+
+
+    <property name="acs-api-key-development" type="string">YOUR-KEY-HERE</property>
+
+Additional details can be found in the chapter of the book
+
+----------
+
+Book is available at 
+* [Barnes & Noble](http://www.barnesandnoble.com/w/building-cross-platform-apps-using-titanium-alloy-and-appcelerator-cloud-services-aaron-saunders/1119684143?ean=9781118673256) 
+* [Amazon](http://www.amazon.com/Building-Cross-Platform-Titanium-Appcelerator-Services/dp/1118673255)
+* [Wiley](http://www.wiley.com/WileyCDA/WileyTitle/productCd-1118673255.html)
+
+----------------------------------
+
+A more detailed and complete example is outlined here [in my book on Appcelerator Alloy Development](https://github.com/aaronksaunders/AppC-Alloy-Book)
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,11 +1,46 @@
-function doClick(e) {
-	
-	
-	// this is available from your appcelerator cloud services console
-	var appId =  "[your-app-id]";
-	
-	
-	var url = "https://api.cloud.appcelerator.com/v1/users/login.json?key=" +appId;
+/**
+ * MUST include the library or this will fail
+ */
+var Cloud = require('ti.cloud');
+
+/**
+ * this is a test to create a user using the ACS ti.cloud module. When you created your
+ * application and "Cloud Enabled" it by clicking the button in the tiapp.xml 
+ * properties page, the library was added to you app.
+ * 
+ * made sure you have created you application in the ACS console or your test app
+ * will not work properly
+ */
+function doACSTest(e) {
+
+	Cloud.Users.create({
+		email : 'test@mycompany.com',
+		first_name : 'test_firstname',
+		last_name : 'test_lastname',
+		password : 'test_password',
+		password_confirmation : 'test_password'
+	}, function(e) {
+		if (e.success) {
+			// success
+			Ti.API.info(JSON.stringify(e, null, 2));
+		} else {
+			Ti.API.error(JSON.stringify(e, null, 2));
+		}
+	});
+
+}
+
+/**
+ *
+ * @param {Object} e
+ */
+function doHTTPTest(e) {
+
+	// this is available from your appcelerator cloud services console or from your
+	// tiapp.xml
+	var appId =  Ti.App.Properties.getString("acs-api-key-development"); //"[your-app-id]";
+
+	var url = "https://api.cloud.appcelerator.com/v1/users/login.json?key=" + appId;
 	var client = Ti.Network.createHTTPClient({
 		//  called when the response data is available
 		onload : function(_response) {
@@ -21,9 +56,8 @@ function doClick(e) {
 	});
 	// Prepare the connection
 	client.open("POST", url);
-	
-	
-	// Send the request with parameters, be sure to create this admin
+
+	// Send the request with parameters, be sure to create this user
 	// account in your Appcelerator CLoud Services console before testing
 	// the project
 	client.send({
