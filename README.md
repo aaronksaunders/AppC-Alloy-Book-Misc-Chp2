4ce2d86779440426cd49b3686c3d31d4989f29a7
aaronksaunders <aaron@clearlyinnovative.com>
2015-02-02 20:07:13 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # AppC-Alloy-Book-Misc-Chp2
+# AppC-Alloy-Book-Misc-Chp2
---
+++ app/controllers/index.js
@@ -0,0 +1,36 @@
+function doClick(e) {
+	
+	
+	// this is available from your appcelerator cloud services console
+	var appId =  "[your-app-id]";
+	
+	
+	var url = "https://api.cloud.appcelerator.com/v1/users/login.json?key=" +appId;
+	var client = Ti.Network.createHTTPClient({
+		//  called when the response data is available
+		onload : function(e) {
+			var results = JSON.parse(client.responseText);
+			// display results on console
+			Ti.API.info(JSON.stringify(results, null, 2));
+		},
+		//  called when an error occurs, including a timeout
+		onerror : function(e) {
+			var results = JSON.parse(client.responseText);
+			// display error results on the console
+			Ti.API.err(JSON.stringify(results, null, 2));
+		},
+	});
+	// Prepare the connection
+	client.open("POST", url);
+	
+	
+	// Send the request with parameters, be sure to create this admin
+	// account in your Appcelerator CLoud Services console before testing
+	// the project
+	client.send({
+		login : "wileyonetest@clearlyinnovative.com",
+		password : "wileyonetest"
+	});
+}
+
+$.index.open();
